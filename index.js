39db13c6d2ac80515b2684227bb85de97c9e29b3
Arthur <45287989+kovert99@users.noreply.github.com>
2019-05-30 10:59:37 +0300

    ...

--- index.js
+++ index.js
@@ -1,14 +1,75 @@
 const { json } = require('micro');
+const axios = require('axios');
 
-module.exports = async req => {
+require('dotenv').config();
+
+module.exports = async (req, res) => {
 	const { request, session, version } = await json(req);
+	const { command } = request;
 
-	return {
+	let vehicleNumber, clearVehicleNumber, responseText;
+
+	if (!command) responseText = 'Здравствуйте, я помогу вам найти местонахождение автомобиля по его гос-номеру. Например, спросите меня: где к 123 м н';
+	else {
+		commandParts = command.split('где');
+
+		if (commandParts[1]) {
+			vehicleNumber = commandParts[1].toString();
+			clearVehicleNumber = vehicleNumber.split(' ').join('');
+			
+			if (!clearVehicleNumber || clearVehicleNumber.length != 6) responseText = 'Гос-номер автомобиля указан не верно. Пример правильного номера: к 123 м н';
+			else {
+				// демо-данные
+				const vehicle = {
+					'к123мн': {
+						'coordinates': [52.297113, 54.901383]
+					},
+					'а321ав': {
+						'coordinates': [52.296503, 54.894555]
+					},
+					'р456ук': {
+						'coordinates': [52.304791, 54.896724]
+					},
+					'к201рв': {
+						'coordinates': [52.317364, 54.896470]
+					}
+				}[clearVehicleNumber];
+
+				if (!vehicle) responseText = 'В нашей базе данных нет автомобиля с гос-номером: ' + vehicleNumber;
+				else {
+					let GeoObject;
+console.log('process.env', process.env);
+					
+					try {
+						// получаем адрес по координатам
+						const { data } = await axios.get('https://geocode-maps.yandex.ru/1.x/?apikey=' + process.env.YANDEX_MAPS_API_KEY + '&geocode=' + vehicle.coordinates.join(',') + '&format=json');
+						
+						if (data.response && data.response.GeoObjectCollection) {
+							const { featureMember } = data.response.GeoObjectCollection;
+
+							// получаем улицу объекта
+							if (featureMember[0]) GeoObject = featureMember[0].GeoObject;
+						};
+					} catch (error) { 
+						console.log(error); 
+					};
+
+					if (!GeoObject) responseText = 'Координаты автомобиля: ' + vehicle.coordinates.join(', ');
+					else responseText = 'Автомобиль находится по адресу: ' + GeoObject.name + ' [' + vehicle.coordinates.join(', ') + ']';
+				};
+			};
+		} else responseText = 'Я вас не понял.';
+	};
+
+	const result = {
 		version,
 		session,
 		response: {
-			text: request.original_utterance || 'Hello world!',
-			end_session: false,
-		},
+			text: responseText,
+			end_session: false
+		}
 	};
+
+	res.write(JSON.stringify(result));
+	res.end();
 };
